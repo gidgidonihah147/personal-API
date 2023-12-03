be5881ec726010520e8e7af313c57429a3fe1715
TristinParker <gidgidonihah@byui.edu>
2023-12-02 19:38:41 -0700

    Update login.test.js

--- test/login.test.js
+++ test/login.test.js
@@ -20,6 +20,7 @@ it("Should get a login token",async ()=>{
     console.log("Token "+token);
     const status = response.status;
 
+    
     expect(status).toBe(200);
     expect(token.length).toBe(36);
 
